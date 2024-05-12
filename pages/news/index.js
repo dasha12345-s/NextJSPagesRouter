d798cd783b17a81769844d9673e25f326bcf4c78
Darya <shostak19@icloud.com>
2024-05-12 13:15:07 -0400

    Linking Between Pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,18 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 function NewsPage(){
-  return<h1>
+  return <Fragment>
+    <h1>
     The NEws Page
   </h1>
+  <ul>
+    <li>
+      <Link href='/news/about'>About</Link>
+    </li>
+    <li>Something</li>
+  </ul>
+  </Fragment>
 }
 
 export default NewsPage;
